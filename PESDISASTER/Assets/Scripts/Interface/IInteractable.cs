06dcdd5a5a1d968dfab8fa45ded3372827633dd8
nakayama_yuto <udoyama5@gmail.com>
2026-03-05 21:06:55 +0900

    [add]Added an interface for item acquisition events.
    変更内容：アイテム取得イベント用のインターフェースを追加した。

---
+++ PESDISASTER/Assets/Scripts/Interface/IInteractable.cs
@@ -0,0 +1,19 @@
+namespace PESDISASTER
+{
+    /// <summary>
+    /// インタラクト可能なオブジェクトが実装するインターフェース
+    /// </summary>
+    public interface IInteractable
+    {
+        /// <summary>
+        /// インタラクトしたときの処理を行う関数
+        /// </summary>
+        public void Interact();
+
+        /// <summary>
+        /// アイテムの説明文を返す関数
+        /// </summary>
+        /// <returns></returns>
+        public string GetInteractText();
+    }
+}
