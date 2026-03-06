e098e89cec571f8d573ac316b541f45453bf0927
nakayama_yuto <udoyama5@gmail.com>
2026-03-06 12:13:30 +0900

    [implement]I implemented a class for picking up items.
    変更内容：アイテムを拾うためのクラスを実装した。

---
+++ PESDISASTER/Assets/Scripts/Class/PickupItem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace PESDISASTER
+{
+    /// <summary>
+    /// アイテムを拾うためのクラス
+    /// </summary>
+    public class PickupItem : MonoBehaviour, IInteractable
+    {
+        /// <summary>
+        /// アイテムの名前の変数
+        /// </summary>
+        [SerializeField]
+        private string itemName = "ハンドガン";
+
+        /// <summary>
+        /// 拾うときの処理を行う関数
+        /// </summary>
+        public void Interact()
+        {
+            Destroy(gameObject);// シーン上からアイテムを消す
+        }
+
+        /// <summary>
+        /// 拾ったアイテムの名前を表示する関数
+        /// </summary>
+        /// <returns></returns>
+        public string GetInteractText()
+        {
+            return $"{itemName} を拾う";// 拾ったアイテムの名前を表示する
+        }
+    }
+}
