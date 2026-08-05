592d398d44990fa95639101a59092396005c0461
nakayama_yuto <udoyama5@gmail.com>
2026-08-05 17:07:55 +0900

    [fix]Fixed an animation bug that occurred during the walkthrough.
    変更内容：攻略ナビ時のアニメーションバグを修正した。

--- PESDISASTER/Assets/Scripts/Class/PlayerNoticeUI_Manager.cs
+++ PESDISASTER/Assets/Scripts/Class/PlayerNoticeUI_Manager.cs
@@ -13,40 +13,46 @@ namespace PESDISASTER
         /// </summary>
         [SerializeField]
         private Transform _navigateNoticeUI_Target;
+        /// <summary>
+        /// ゲーム目的UIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _gameRuleUI_Target;
+        /// <summary>
+        /// マガジン空っぽUIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _magazineEmptyUI_Target;
+        /// <summary>
+        /// リロード完了UIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _reloadCompleteUI_Target;
+        /// <summary>
+        /// リロード失敗UIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _reloadFailedUI_Target;
+        /// <summary>
+        /// ロック中UIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _lockedUI_Target;
+        /// <summary>
+        /// 棚の開け方UIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _openShelfTutorial_UI_Target;
+        /// <summary>
+        /// 棚の開け方UIのターゲットを参照する変数
+        /// </summary>
+        [SerializeField]
+        private Transform _damageNoticeUI_Target;
 
         /// <summary>
         /// アニメーターを参照する変数
         /// </summary>
-        private Animator animator;
-
-        /// <summary>
-        /// ゲーム目的UIのターゲットを参照する変数
-        /// </summary>
-        public Transform gameRuleUI_Target;
-        /// <summary>
-        /// マガジン空っぽUIのターゲットを参照する変数
-        /// </summary>
-        public Transform magazineEmptyUI_Target;
-        /// <summary>
-        /// リロード完了UIのターゲットを参照する変数
-        /// </summary>
-        public Transform reloadCompleteUI_Target;
-        /// <summary>
-        /// リロード失敗UIのターゲットを参照する変数
-        /// </summary>
-        public Transform reloadFailedUI_Target;
-        /// <summary>
-        /// ロック中UIのターゲットを参照する変数
-        /// </summary>
-        public Transform lockedUI_Target;
-        /// <summary>
-        /// 棚の開け方UIのターゲットを参照する変数
-        /// </summary>
-        public Transform openShelfTutorial_UI_Target;
-        /// <summary>
-        /// 棚の開け方UIのターゲットを参照する変数
-        /// </summary>
-        public Transform damageNoticeUI_Target;
+        private Animator _animator;
 
         /// <summary>
         /// クラス自身のインスタンスを参照する変数
@@ -56,48 +62,53 @@ namespace PESDISASTER
         /// <summary>
         /// アニメーターのゲーム目的トリガーを参照する変数
         /// </summary>
-        public static readonly int ruleTrigger = Animator.StringToHash("OnRule");
+        private static readonly int _ruleTrigger_ID = Animator.StringToHash("OnRule");
         /// <summary>
         /// アニメーターのマガジン空っぽトリガーを参照する変数
         /// </summary>
-        public static readonly int emptyTrigger = Animator.StringToHash("OnEmpty");
+        private static readonly int _emptyTrigger_ID = Animator.StringToHash("OnEmpty");
         /// <summary>
         /// アニメーターのリロード完了トリガーを参照する変数
         /// </summary>
-        public static readonly int reloadTrigger = Animator.StringToHash("OnReload");
+        private static readonly int _reloadTrigger_ID = Animator.StringToHash("OnReload");
         /// <summary>
         /// アニメーターのリロード失敗トリガーを参照する変数
         /// </summary>
-        public static readonly int failedTrigger = Animator.StringToHash("OnFailed");
+        private static readonly int _failedTrigger_ID = Animator.StringToHash("OnFailed");
         /// <summary>
         /// アニメーターのロック中トリガーを参照する変数
         /// </summary>
-        public static readonly int lockedTrigger = Animator.StringToHash("OnLocked");
+        private static readonly int _lockedTrigger_ID = Animator.StringToHash("OnLocked");
         /// <summary>
         /// アニメーターの棚の開け方トリガーを参照する変数
         /// </summary>
-        public static readonly int openTutorialTrigger = Animator.StringToHash("OnOpenTutorial");
+        private static readonly int _openTutorialTrigger_ID = Animator.StringToHash("OnOpenTutorial");
         /// <summary>
         /// アニメーターのダメージ時トリガーを参照する変数
         /// </summary>
-        public static readonly int damageTrigger = Animator.StringToHash("OnDamage");
+        private static readonly int _damageTrigger_ID = Animator.StringToHash("OnDamage");
         /// <summary>
         /// アニメーターの攻略ナビ時トリガーを参照する変数
         /// </summary>
-        private static readonly int _navigateTrigger = Animator.StringToHash("OnNavigate");
+        private static readonly int _navigateTrigger_ID = Animator.StringToHash("OnNavigate");
 
         /// <summary>
         /// 通知アニメーションの時間を参照する変数
         /// </summary>
-        private float noticeAnimTime = 2f;
+        private float _noticeAnimTime = 2f;
         /// <summary>
         /// 棚の開け方アニメーションの時間を参照する変数
         /// </summary>
-        private float openTutorialAnimTime = 3f;
+        private float _openTutorialAnimTime = 3f;
         /// <summary>
         /// ダメージ時アニメーションの時間を参照する変数
         /// </summary>
-        private float damageAnimTime = 1f;
+        private float _damageAnimTime = 1f;
+
+        /// <summary>
+        /// アニメーション中かどうかを参照する変数
+        /// </summary>
+        private bool _isAnimating = false;
 
         /// <summary>
         /// 初期設定を行う関数
@@ -113,38 +124,37 @@ namespace PESDISASTER
             {
                 Destroy(gameObject);
             }
+
             // コンポーネントの登録
-            animator = GetComponent<Animator>();
+            _animator = GetComponent<Animator>();
+
             // 最初はUIを非表示
-            Hide();
+            ShowHide(false);
         }
 
         /// <summary>
-        /// UIを非表示にする関数
+        /// 全てのUIを表示・非表示にする関数
         /// </summary>
-        private void Hide()
+        /// <param name="isActive"></param>
+        private void ShowHide(bool isActive)
         {
+            // すべての子オブジェクトを参照
             foreach (Transform child in transform)
             {
+                // 子オブジェクトを非表示にする
                 child.gameObject.SetActive(false);
             }
         }
 
         /// <summary>
-        /// UIを指定して表示する関数
-        /// </summary>
-        public void TargetShow(Transform target)
-        {
-            target.gameObject.SetActive(true);
-        }
-
-        /// <summary>
-        /// UIを指定して非表示にする関数
+        /// 指定のUIを表示・非表示する関数
         /// </summary>
         /// <param name="target"></param>
-        public void TargetHide(Transform target)
+        /// <param name="isActive"></param>
+        public void TargetShowHide(Transform target, bool isActive)
         {
-                target.gameObject.SetActive(false);
+            // 指定のUIを表示・非表示する
+            target.gameObject.SetActive(isActive);
         }
 
         /// <summary>
@@ -152,8 +162,10 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeRule()
         {
-            TargetShow(gameRuleUI_Target);
-            animator.SetTrigger(ruleTrigger);// アニメーターのゲーム目的トリガーを発動
+            // 指定のUIを表示する
+            TargetShowHide(_gameRuleUI_Target, true);
+            // アニメーターのゲーム目的トリガーを発動
+            _animator.SetTrigger(_ruleTrigger_ID);
         }
 
         /// <summary>
@@ -161,7 +173,8 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeEmpty()
         {
-            StartCoroutine(NoticeAnimCoroutine(magazineEmptyUI_Target, emptyTrigger,noticeAnimTime));// 通知のアニメーションを行う
+            // 通知のアニメーションを行う
+            StartCoroutine(NoticeAnimCoroutine(_magazineEmptyUI_Target, _emptyTrigger_ID, _noticeAnimTime));
         }
 
         /// <summary>
@@ -169,7 +182,8 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeReloadComplete()
         {
-            StartCoroutine(NoticeAnimCoroutine(reloadCompleteUI_Target, reloadTrigger,noticeAnimTime));// 通知のアニメーションを行う
+            // 通知のアニメーションを行う
+            StartCoroutine(NoticeAnimCoroutine(_reloadCompleteUI_Target, _reloadTrigger_ID, _noticeAnimTime));
         }
 
         /// <summary>
@@ -177,7 +191,8 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeReloadFailed()
         {
-            StartCoroutine(NoticeAnimCoroutine(reloadFailedUI_Target, failedTrigger,noticeAnimTime));// 通知のアニメーションを行う
+            // 通知のアニメーションを行う
+            StartCoroutine(NoticeAnimCoroutine(_reloadFailedUI_Target, _failedTrigger_ID, _noticeAnimTime));
         }
 
         /// <summary>
@@ -188,15 +203,31 @@ namespace PESDISASTER
         /// <returns></returns>
         private IEnumerator NoticeAnimCoroutine(Transform target, int triggerNumber, float time)
         {
-            if (!target == damageNoticeUI_Target)
+            // もしアニメーション中の場合
+            if (_isAnimating)
+            {
+                yield break;
+            }
+
+            // もしダメージ通知UI以外の通知の場合
+            if (!target == _damageNoticeUI_Target)
             {
                 AudioManager.Instance.PlaySE("Notice");
             }
 
-            TargetShow(target);
-            animator.SetTrigger(triggerNumber);
+            // --- 通知アニメーション処理 ---
+            // アニメーション中フラグをオンにする
+            _isAnimating = true;
+            // 指定のUIを表示する
+            TargetShowHide(target, true);
+            // 指定のトリガーを発動する
+            _animator.SetTrigger(triggerNumber);
+            // 指定の時間だけ待機する
             yield return new WaitForSeconds(time);
-            TargetHide(target);
+            // 指定のUIを非表示にする
+            TargetShowHide(target, false);
+            // アニメーション中フラグをオフにする
+            _isAnimating = false;
         }
 
         /// <summary>
@@ -204,7 +235,8 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeLocked()
         {
-            StartCoroutine(NoticeAnimCoroutine(lockedUI_Target, lockedTrigger,noticeAnimTime));// 通知のアニメーションを行う
+            // 通知のアニメーションを行う
+            StartCoroutine(NoticeAnimCoroutine(_lockedUI_Target, _lockedTrigger_ID, _noticeAnimTime));
         }
 
         /// <summary>
@@ -212,7 +244,8 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeOpenShelfTutorial()
         {
-            StartCoroutine(NoticeAnimCoroutine(openShelfTutorial_UI_Target, openTutorialTrigger, openTutorialAnimTime));// 通知のアニメーションを行う
+            // 通知のアニメーションを行う
+            StartCoroutine(NoticeAnimCoroutine(_openShelfTutorial_UI_Target, _openTutorialTrigger_ID, _openTutorialAnimTime));
         }
 
         /// <summary>
@@ -220,7 +253,8 @@ namespace PESDISASTER
         /// </summary>
         public void NoticeDamage()
         {
-            StartCoroutine(NoticeAnimCoroutine(damageNoticeUI_Target, damageTrigger, damageAnimTime));// ダメージ通知のアニメーションを行う
+            // ダメージ通知のアニメーションを行う
+            StartCoroutine(NoticeAnimCoroutine(_damageNoticeUI_Target, _damageTrigger_ID, _damageAnimTime));
         }
 
         /// <summary>
@@ -229,7 +263,7 @@ namespace PESDISASTER
         public void NoticeNavigate()
         {
             // 攻略ナビ通知のアニメーションを行う
-            StartCoroutine(NoticeAnimCoroutine(_navigateNoticeUI_Target, _navigateTrigger, noticeAnimTime));
+            StartCoroutine(NoticeAnimCoroutine(_navigateNoticeUI_Target, _navigateTrigger_ID, _noticeAnimTime));
         }
     }
 }
